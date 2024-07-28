9efe0a131cf8711f8a221f8beac4f9d30c4946df
Bi7al <bilal.muhammad4573@gmail.com>
2024-07-28 17:10:14 +0500

    Added Functionality To Add Device

--- src/Components/Devices/Devices.jsx
+++ src/Components/Devices/Devices.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import "./Devices.css"
 
 function Devices() {
@@ -6,7 +6,7 @@ function Devices() {
     //Can also Use Multiple states for multiple device types
     const [devices, setDevices] = useState({
         Lights: {
-            count: 6,
+
             type: 'Lamps',
             devices: [
                 { id: 1, name: 'Living Room Lamp' },
@@ -15,6 +15,7 @@ function Devices() {
 
 
             ],
+            count: 3,
         },
         Heaters: {
             count: 2,
@@ -29,90 +30,126 @@ function Devices() {
             devices: [],
         },
     });
-    //* PLACE HOLDER CODE SECTION
-    //! Initialize new device input state
-    // const [newDevice, setNewDevice] = useState('');
+
+    const [newDevice, setNewDevice] = useState({
+        id: 0,
+        deviceName: '',
+        deviceType: '',
+    });
+    function onChange(e) {
+        setNewDevice({ ...newDevice, [e.target.name]: e.target.value });
+    }
+    const ClseBtn = useRef();
+
+
 
     // Function to add a new device
-    const addDevice = () => {
-        //!Left As placeholder
-        // // Check if input is not empty
-        // if (newDevice.trim() !== '') {
-        //     // Extract type and count from input
-        //     const type = newDevice.split(' ')[0];
-        //     const count = parseInt(newDevice.split(' ')[1]);
+    function addNewDevice(event) {
+        event.preventDefault();
+        setNewDevice((prev) => {
+            return { ...prev, id: new Date().getTime() }
+        })
+        setDevices((prev) => ({
+            ...prev,
+            [newDevice.deviceType]: {
+                ...prev[newDevice.deviceType],
+                devices: [...prev[newDevice.deviceType].devices, {
+                    id: newDevice.id,
+                    name: newDevice.deviceName
+                }],
+                count: prev[newDevice.deviceType].count + 1
+            },
 
-        //     // Update devices state with new device
-        //     setDevices((prevDevices) => ({
-        //         ...prevDevices,
-        //         [type.toLowerCase()]: {
-        //             count,
-        //             type,
-        //             devices: [],
-        //         },
-        //     }));
+        }))
+        setNewDevice({
+            id: 0,
+            deviceName: '',
+            deviceType: '',
+        });
+        ClseBtn.current.click();
 
-        //     // Reset new device input
-        //     setNewDevice('');
-        // }
     };
 
     // Function to assign device to room
     function assignDeviceToRoom(deviceId, roomId) {
-        //!Place holder code
-        // Update devices state with assigned room
-        // setDevices((prevDevices) => {
-        //     const deviceType = Object.keys(prevDevices).find((type) => prevDevices[type].devices.find((device) => device.id === deviceId));
-        //     const deviceIndex = prevDevices[deviceType].devices.findIndex((device) => device.id === deviceId);
-        //     prevDevices[deviceType].devices[deviceIndex].room = roomId;
-        //     return prevDevices;
-        // });
+
     };
 
     return (
-        <div className="device-wrapper">
-            <h1>List of devices</h1>
-            <div className="addbtn">
-                <button>Add a Device</button>
-            </div>
-            {/* Render device list */}
-            <div className="device-list-container">
-                {Object.keys(devices).map((key, index) => (
-                    <div className="devices" key={index}>
-                        <div className="header">
-                            <button className={`collapsed ${key} header`} type="button" data-bs-toggle="collapse" data-bs-target={`#${key}`} aria-expanded="true">
-                                <h5>{key}</h5>
-                            </button>
-                        </div>
-                        <div className="accordianItem">
-                            <div id={key} className="collapse">
-                                <div className="accordion-body">
-                                    {devices[key].type && (
-                                        <p>
-                                            <strong> {devices[key].type}: {devices[key].count}</strong>
-                                        </p>
-                                    )}
-                                    <ul className='device-listing'>
-                                        {devices[key].devices.map((device, index) => (
-                                            <li key={index} >
-                                                <button className='device-btn mb-1' onClick={() => console.log(`Device ${device.name} clicked!`)}>{device.name}</button>
-                                                <select className='opt-btn' value={device.room} onChange={(e) => assignDeviceToRoom(device.id, e.target.value)}>
-                                                    <option value="">Assign to room</option>
-                                                    <option value="Living Room">Living Room</option>
-                                                    <option value="Kitchen">Kitchen</option>
-                                                    <option value="Bedroom">Bedroom</option>
-                                                </select>
-                                                <button className='ms-1 ps-1 border-0 opt-btn' onClick={(e) => console.log(e.target)}>Do Something</button>
-                                            </li>
-                                        ))}
-                                    </ul>
+        <>
+            <div className="device-wrapper">
+                <h1>List of devices</h1>
+                <div className="addbtn">
+                    <button data-bs-toggle="modal" data-bs-target='#input-modal-new-device'>Add a Device</button>
+                </div>
+                {/* Render device list */}
+                <div className="device-list-container">
+                    {Object.keys(devices).map((key, index) => (
+                        <div className="devices" key={index}>
+                            <div className="header">
+                                <button className={`collapsed ${key} header`} type="button" data-bs-toggle="collapse" data-bs-target={`#${key}`} aria-expanded="true">
+                                    <h5>{key}</h5>
+                                </button>
+                            </div>
+                            <div className="accordianItem">
+                                <div id={key} className="collapse">
+                                    <div className="accordion-body">
+                                        {devices[key].type && (
+                                            <p>
+                                                <strong> {devices[key].type}: {devices[key].count}</strong>
+                                            </p>
+                                        )}
+                                        <ul className='device-listing'>
+                                            {devices[key].devices.map((device, index) => (
+                                                <li key={index} >
+                                                    <button className='device-btn mb-1' onClick={() => console.log(`Device ${device.name} clicked!`)}>{device.name}</button>
+                                                    <select className='opt-btn' value={device.room} onChange={(e) => assignDeviceToRoom(device.id, e.target.value)}>
+                                                        <option value="">Assign to room</option>
+                                                        <option value="Living Room">Living Room</option>
+                                                        <option value="Kitchen">Kitchen</option>
+                                                        <option value="Bedroom">Bedroom</option>
+                                                    </select>
+                                                    <button className='ms-1 ps-1 border-0 opt-btn' onClick={(e) => console.log(e.target)}>Do Something</button>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
+
+
+            {/* MOdal to add a new Device */}
+            <div className="modal fade" id="input-modal-new-device" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+                    <div className="modal-content p-3 height w-75">
+                        <form action="" onSubmit={addNewDevice}>
+                            <div className="device-type-input mb-3">
+                                Select Device type: {
+                                    Object.keys(devices).map((key, index) => {
+                                        return (
+                                            <div key={index} className="option-device-type ">
+                                                <input type="radio" id='heater' name='deviceType' value={key} onChange={onChange} required />
+                                                <label htmlFor="heater">{key}</label>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className="device-name-input">
+                                <label htmlFor="device-name" className='text-muted'>Enter Name for the Device:</label>
+                                <input type="text" className='p-1 w-50' id="device-name" name="deviceName" value={newDevice.deviceName} onChange={onChange} placeholder="Enter Device Name" required />
+                            </div>
+                            <button className='new-device-submit-btn'>Submit</button>
+                            <button hidden data-bs-dismiss="modal" ref={ClseBtn}></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
 
     );
 }
